9f2a96726ae488b4d5ca6bde8c782aee422ed2dd
Justin Guze <jaguze@gmail.com>
2015-03-08 18:33:26 -0700

    Even more tests tests

--- Project2/project2/project2/tests/test021_publish_round_robin.cpp
+++ Project2/project2/project2/tests/test021_publish_round_robin.cpp
@@ -5,3 +5,53 @@
  *  Author: jaguz_000
  */ 
 
+#ifdef USE_TEST_021
+
+/************************************************************************/
+/* Expected: T021;1;2;3;4;5;100;6;100;                                                  */
+/************************************************************************/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "../trace/trace.h"
+
+SERVICE * s;
+
+void rr_task_3()
+{
+    add_to_trace(3);
+    Service_Publish(s, 1);
+    add_to_trace(4);
+}
+
+void rr_task_2()
+{
+    int16_t v;
+    add_to_trace(2);
+    Service_Subscribe(s, &v);
+    add_to_trace(6);
+    add_to_trace(v);
+    print_trace();
+}
+
+void rr_task_1()
+{
+    int16_t v;
+    add_to_trace(1);
+    Service_Subscribe(s, &v);
+    add_to_trace(5);
+    add_to_trace(v);
+}
+
+int r_main()
+{
+    set_trace_test(21);
+    s = Service_Init();
+    Task_Create_RoundRobin(rr_task_1, 0);
+    Task_Create_RoundRobin(rr_task_2, 0);
+    Task_Create_RoundRobin(rr_task_3, 0);
+    return 0;
+}
+
+#endif
