d74296a3389bfae4bf2ac74a972ad9978f503b44
Justin Guze <jaguze@gmail.com>
2015-03-23 16:13:56 -0700

    Adding improved analogue basestation code

--- P3_BaseStation/project3/basestation/basestation.cpp
+++ P3_BaseStation/project3/basestation/basestation.cpp
@@ -24,9 +24,12 @@
 #define RADIO_HIGH (PORTB = (uint8_t)RADIO_VCC_PIN)
 
 #define RADIO_SEND_DEBUG_PIN (uint8_t) (_BV(PA0))
-#define INIT_DEBUG_LEDS (DDRA = RADIO_SEND_DEBUG_PIN)
+#define RED_DEBUG_PIN (uint8_t) (_BV(PA1))
+#define INIT_DEBUG_LEDS (DDRA = RADIO_SEND_DEBUG_PIN) // | RED_DEBUG_PIN)
 #define RADIO_SEND_DEBUG_OFF PORTA = (uint8_t)(0)
 #define RADIO_SEND_DEBUG_ON PORTA = (uint8_t)(RADIO_SEND_DEBUG_PIN)
+#define RED_DEBUG_OFF PORTA = (uint8_t)(0)
+#define RED_DEBUG_ON PORTA = (uint8_t)(RED_DEBUG_PIN)
 
 #define HIGH_BYTE(x) x>>8
 #define LOW_BYTE(x) x & 0x00FF
@@ -52,6 +55,20 @@ void setup_radio()
     Radio_Configure_Rx(RADIO_PIPE_0, radio_addr, ENABLE);
     Radio_Configure(RADIO_2MBPS, RADIO_HIGHEST_POWER);
     Radio_Set_Tx_Addr(ROOMBA_ADDRESSES[radio_target]);
+}
+
+void setup_joysticks()
+{
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Set ADC prescalar to 128 - 125KHz sample rate @ 16MHz
+
+    ADMUX |= (1 << REFS0); // Set ADC reference to AVCC
+    ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading
+
+    // No MUX values needed to be changed to use ADC0
+
+    ADCSRA |= (1 << 5);  // Set ADC to Free-Running Mode
+    ADCSRA |= (1 << ADEN);  // Enable ADC
+    ADCSRA |= (1 << ADSC);  // Start A2D Conversions
 }
 
 void set_sender_address(uint8_t * sender_address, uint8_t * address, int length)
@@ -84,6 +101,64 @@ void send_packet()
     }
 }
 
+void turn_on(int i)
+{
+    if (!i)
+    {
+      //  RADIO_SEND_DEBUG_ON;
+    } else if (i)
+    {
+        //RED_DEBUG_ON;
+    }
+    RADIO_SEND_DEBUG_ON;
+}
+
+void read_adc(int i)
+{
+    if (ADCH < 100)
+    {
+        turn_on(i);
+    } else if (ADCH > 156)
+    {
+        turn_on(i);
+    } else {
+        RADIO_SEND_DEBUG_OFF;
+      //  RED_DEBUG_OFF;
+    }
+}
+
+void poll_joysticks()
+{
+    int i = 0;
+    while(1)
+    {
+       ADMUX = 0;
+       ADMUX |= (1 << REFS0); // Set ADC reference to AVCC
+       ADMUX |= (1 << ADLAR); // Left adjust ADC result to allow easy 8 bit reading    
+       for(i = 0; i < 1; i++)
+       {
+           // ADMUX &= (uint8_t) 240;
+         //   ADMUX |= (uint8_t) i; 
+         //   add_to_trace(ADMUX);
+         read_adc(i);
+         
+         ADMUX = 0xC0 | (uint8_t)i;
+        // Task_Next();
+        // ADCSRA |= (1 << ADSC);
+       }
+       // print_trace();
+       Task_Next();
+    }
+}
+
+void turn_off()
+{
+    while(1)
+    {
+        Task_Next();
+    }
+}
+
 void radio_rxhandler(uint8_t pipe_number)
 {
     rxflag = 1;
@@ -92,8 +167,13 @@ void radio_rxhandler(uint8_t pipe_number)
 int r_main()
 {
     INIT_DEBUG_LEDS;
+    RADIO_SEND_DEBUG_OFF;
     Task_Create_System(setup_radio, 0);
-    Task_Create_Periodic(send_packet, 0, 100, 50, 5);
+    Task_Create_System(setup_joysticks, 0);
+   // Task_Create_Periodic(send_packet, 0, 100, 50, 5);
+    Task_Create_Periodic(poll_joysticks, 0, 5, 1, 10);
+   // Task_Create_Periodic(turn_off, 0, 100, 50, 60);
+
     return 0;
 }
 
